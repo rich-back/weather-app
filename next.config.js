6acc988df97fc85eeacaefb5f7f460cb6193e5e8
rich-back <backhouse.richard@gmail.com>
2023-05-15 20:01:39 +0100

    🚧 Initial setup/homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['@tremor/react'],
+    },
+    }
 module.exports = nextConfig
