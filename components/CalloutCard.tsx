37f4ae8477a567ec46f1d765e5a57ff24f88b802
rich-back <backhouse.richard@gmail.com>
2023-05-22 17:48:15 +0100

    ♻️ Updated CalloutCard component adding color to parameters

--- components/CalloutCard.tsx
+++ components/CalloutCard.tsx
@@ -1,20 +1,21 @@
 "use client";
 
 import { CheckCircleIcon, ExclamationIcon } from "@heroicons/react/solid";
-import { Callout } from "@tremor/react";
+import { Callout, Color } from "@tremor/react";
 
 type Props = {
   message: string;
   warning?: boolean;
+  color?: Color;
 };
 
-function CalloutCard({ message, warning }: Props) {
+function CalloutCard({ message, warning, color }: Props) {
   return (
     <Callout
       className="mt-4"
       title={message}
       icon={warning ? ExclamationIcon : CheckCircleIcon}
-      color={warning ? "rose" : "teal"}
+      color={color}
     />
   );
 }
